a525689ee5210e4d90a939b1003c5dc2d22c9fa0
tanxinshequn <tanxinshequn@163.com>
2016-06-05 15:37:06 +0800

    start

--- s.sh
+++
@@ -1,1 +0,0 @@
-chmod 777 -R /var/www/html && /etc/init.d/apache2 start && tail -f /var/log/apache2/error.log
---
+++ start.sh
@@ -0,0 +1,1 @@
+chmod 777 -R /var/www/html && chown www-data:www-data -R /var/www/html && /usr/sbin/apache2ctl -D FOREGROUND
